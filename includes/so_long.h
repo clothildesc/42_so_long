cd49bb8fada43af9d13826658cd01a50aa33de4c
Clothilde Scache <cscache@f6r11s6.clusters.42paris.fr>
2025-06-24 09:52:19 +0200

    ex

---
+++ includes/so_long.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cscache <cscache@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/24 09:49:15 by cscache           #+#    #+#             */
+/*   Updated: 2025/06/24 09:50:49 by cscache          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "../libft/libft.h"
+
+#endif
